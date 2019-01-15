768878ad3b6d67610c9822c69e961b9560d6b577
Prudence Codjia <pcodjia@ironguard.net>
2019-01-14 21:24:29 -0500

    friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+var friends = [{
+        name: "Agbota Yan",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "2",
+            "2",
+            "1",
+            "1",
+            "2",
+            "2",
+            "3",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Alao Toba",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "4",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Shola Ayo",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "3",
+            "3",
+            "1",
+            "4",
+            "2",
+            "1",
+            "5",
+            "3",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Cossi Yon",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "4",
+            "3",
+            "1",
+            "2",
+            "4",
+            "2",
+            "3",
+            "4",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Dibo Bola",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "5",
+            "2",
+            "4",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Brou Fifi",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "2",
+            "2",
+            "1",
+            "5",
+            "3",
+            "1",
+            "2",
+            "3",
+            "5",
+            "3"
+        ]
+    },
+    {
+        name: "Amlon Sogbe",
+        photo: "./app/data/public/images/images1.jpeg",
+        scores: [
+            "2",
+            "4",
+            "1",
+            "2",
+            "4",
+            "3",
+            "1",
+            "5",
+            "4",
+            "5"
+        ]
+    }
+];
+
+exports.list = friends;
